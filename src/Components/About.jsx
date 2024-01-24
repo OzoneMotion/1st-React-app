f645c9320e46f068e5f74114762f252e8fddf8bf
OzoneMotion <seovelaz@gmail.com>
2024-01-24 14:10:59 -0600

    Subí pero sin módulos

---
+++ src/Components/About.jsx
@@ -0,0 +1,16 @@
+import './About.css'
+
+function About() {
+    return (
+        <div className='about-container'>
+            <h1>About me</h1>
+            <img src="https://th.bing.com/th/id/R.5cc01588d5429dc52722acad08d7a997?rik=XLtJpe8OwPBQbQ&pid=ImgRaw&r=0" alt="" width={200} />
+            <h2>Jorge Roberto Velazquez Milanés</h2>
+            <p>Me gustan los monitos, las plantas, estudiar y aprender cosas nuevas.</p>
+            <p>No me gusta tomarme fotos, tengo 1 gata blanca que ronronea todo el día.</p>
+            <h4>email: jorge2598@gmail.com</h4>
+        </div>
+    )
+}
+
+export default About
